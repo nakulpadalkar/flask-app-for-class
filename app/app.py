bc5ec6595aae2c2fcc9af06ebebd752ab3d3beb8
nakulpadalkar <2807210+nakulpadalkar@users.noreply.github.com>
2023-11-12 15:54:03 -0500

    removed images and updated more buttons

--- app/app.py
+++ app/app.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 import numpy as np
 import pandas as pd
 import joblib
-from plotly.subplots import make_subplots
-import plotly.graph_objs as go
 import uuid
 import os
 import secrets
@@ -17,28 +15,27 @@ load_dotenv(dotenv_path)
 app = Flask(__name__)
 app.secret_key = secrets.token_hex()
 
-# Specify the directory where the models are saved
-model_directory = "./app/TrainedModel"
+# Define available model names
+available_models = ['lr', 'Ridge', 'Lasso', 'Tree', 'Random Forest', 'Bagging', 'GBM', 'XGB', 'LightGBM']
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_world():
-    prediction_text = None
-
+    prediction_text = None  # Initialize the variable to store prediction text
     if request.method == 'GET':
         print("GET request received")
-        return render_template("index.html", href="baseimage.svg")
+        return render_template("index.html", available_models=available_models)
     else:
         try:
             text = request.form['text']
-            selected_model = request.form['model']  # Get the selected model from the form
+            selected_model = request.form.get('model')
             random_string = uuid.uuid4().hex
-            path = f"./static/{random_string}.svg"
+
             print(f"Received POST request with input data: {text}")
 
-            # Load the selected model from the specified directory
-            model_filename = os.path.join(model_directory, f"{selected_model}_model.joblib")
-            model = joblib.load(model_filename)
-            
+            # Load the selected model...
+            model_path = f"./app/TrainedModel/{selected_model}_model.joblib"
+            model = joblib.load(model_path)
+
             np_arr = floatsome_to_np_array(text)
 
             # Check if the input data has the correct shape (1, 13)...
@@ -52,10 +49,6 @@ def hello_world():
                 prediction_text = f"The predicted value is: {prediction[0]:.2f}"
                 print(f"Prediction: {prediction_text}")
 
-                # Generate plot...
-                plot_graphs(model, np_arr, path)
-                print("Plot generated")
-
             else:
                 flash('Input data is not in the correct format. Please enter 13 comma-separated values.', 'danger')
                 return redirect(url_for('error_page'))
@@ -65,72 +58,17 @@ def hello_world():
             print(f"Error: {str(e)}")
             return redirect(url_for('error_page'))
 
-    # Append a timestamp to the image URL to prevent caching
-    timestamp = int(time.time())  # Get the current timestamp
-    print(f"Timestamp: {timestamp}")
-    return render_template("index.html", href=f"{path}?t={timestamp}", prediction_text=prediction_text)
+    return render_template("index.html", prediction_text=prediction_text)
 
 @app.route("/error")
 def error_page():
     return render_template("error.html")
-
-def plot_graphs(model, new_input_arr, output_file):
-    boston = pd.read_csv("./app/TrainedModel/BostonHousing.csv")
-
-    fig = make_subplots(rows=1, cols=2)
-
-    # Scatter plot for LSTAT vs MEDV
-    fig.add_trace(
-        go.Scatter(x=boston["lstat"], y=boston['medv'], mode='markers',
-                   marker=dict(color="#003366"),
-                   line=dict(color="#003366", width=1)),
-        row=1, col=1
-    )
-
-    # Scatter plot for RM vs MEDV
-    fig.add_trace(
-        go.Scatter(x=boston['rm'], y=boston['medv'], mode='markers',
-                   marker=dict(color="#FF6600"),
-                   line=dict(color="#FF6600", width=1)),
-        row=1, col=2
-    )
-
-    # Predictions
-    new_preds = model.predict(new_input_arr)
-    RM_input = new_input_arr[0][5]
-    LSTAT_input = new_input_arr[0][12]
-
-    # Add predicted points to the plots
-    fig.add_trace(
-        go.Scatter(x=[LSTAT_input], y=new_preds, mode='markers', name="Predicted Output",
-                   marker=dict(color="#FFCC00", size=15),
-                   line=dict(color="#FFCC00", width=1)),
-        row=1, col=1
-    )
-
-    fig.add_trace(
-        go.Scatter(x=[RM_input], y=new_preds, mode='markers', name="Predicted Output",
-                   marker=dict(color="#6600cc", size=15),
-                   line=dict(color="red", width=1)),
-        row=1, col=2
-    )
-
-    # Update axes and layout
-    fig.update_xaxes(title_text="Lower State Population (%)", row=1, col=1)
-    fig.update_xaxes(title_text="Number of Rooms", row=1, col=2)
-    fig.update_yaxes(title_text="House Prices ($1000)", row=1, col=1)
-    fig.update_layout(height=600, width=1400, title_text="Variation in Housing Prices")
-
-    # Save and show the figure
-    fig.write_image(output_file, width=1200, engine="kaleido")
-
 
 def floatsome_to_np_array(floats_str):
     floats = np.array([float(x) for x in floats_str.split(',') if x.strip()], dtype=np.float32)
     if len(floats) != 13:
         raise ValueError("Expected 13 comma-separated values.")
     return floats.reshape(1, 13)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
